26decc563431c64fd88a1a41350aa6f0088a02a5
donche129 <donaldcherestal@gmail.com>
2016-04-23 21:03:38 -0400

    updated sort

--- sorting.rb
+++ sorting.rb
@@ -6,3 +6,17 @@ books.sort! { |firstBook, secondBook| firstBook <=> secondBook }
 # Sort your books in descending order, in-place below
 
 books.sort! { |firstBook, secondBook| secondBook <=> firstBook }
+
+def alphabetize(arr, rev=false)
+    if rev
+        arr.reverse!
+    else
+        arr.sort!
+    end
+end
+
+numbers = [5, 1, 3, 8]
+
+alphabetize(numbers, true)
+
+puts numbers
